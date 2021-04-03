4a02124787a2959827fefb11d9565755e09a4c9e
zkan <zkan.cs@gmail.com>
2021-04-03 18:51:45 +0700

    Set up Pytest :bear:

--- survey/survey/questions/apps.py
+++ survey/survey/questions/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class QuestionsConfig(AppConfig):
-    name = 'questions'
+    name = 'survey.questions'
+    verbose_name = _('Questions')

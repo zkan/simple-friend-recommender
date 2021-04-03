56a026b31da98a1ad1b07313d456b25c67556b83
zkan <zkan.cs@gmail.com>
2021-04-03 08:43:35 +0700

    Restructure Django project to follow Cookiecutter Django :bear:

--- survey/config/settings.py
+++ survey/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'survey.questions.apps.QuestionsConfig',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'survey.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'survey.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
--- survey/manage.py
+++ survey/manage.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'survey.settings')
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            'available on your PYTHONPATH environment variable? Did you '
+            'forget to activate a virtual environment?'
         ) from exc
+
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / 'survey'))
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()

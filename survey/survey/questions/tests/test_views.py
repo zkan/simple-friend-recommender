3afe4d1353b30eddf6425227534eb0fdc8e57403
zkan <zkan.cs@gmail.com>
2021-04-04 11:39:13 +0700

    Redirect to question 1 when input name in question 0 :bear:

--- survey/survey/questions/tests/test_views.py
+++ survey/survey/questions/tests/test_views.py
@@ -3,22 +3,35 @@ from django.urls import reverse
 
 
 class TestQuestion0View(TestCase):
-    def test_question_0_view_should_have_some_random_text(self):
-        url = reverse('questions:question_0')
-        response = self.client.get(url)
-        assert 'Question 0' in str(response.content)
+    def setUp(self):
+        self.url = reverse('questions:question_0')
+
+    def test_question_0_view_should_have_title(self):
+        response = self.client.get(self.url)
+        assert '<title>Question 0</title>' in str(response.content)
 
     def test_question_0_view_should_have_form_for_submitting_name(self):
-        url = reverse('questions:question_0')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         expected = '<form action="." method="GET">' \
             '<div class="mb-3">' \
             '<label for="name" class="form-label">Name</label>' \
-            '<input id="name" type="text" class="form-control" />' \
+            '<input id="name" type="text" class="form-control" name="name" />' \
             '</div>' \
             '<button type="submit" class="btn btn-primary">Submit</button>' \
             '</form>'
         assert expected in str(response.content)
+
+    def test_question_0_view_should_redirect_when_input_name(self):
+        name = 'Kan'
+        response = self.client.get(self.url + f'?name={name}')
+
+        assert response.status_code == 302
+
+    def test_question_0_view_should_redirect_to_question_1_when_input_name(self):
+        name = 'Kan'
+        response = self.client.get(self.url + f'?name={name}', follow=True)
+
+        assert '<title>Question 1</title>' in str(response.content)
 
 
 class TestQuestion1View(TestCase):

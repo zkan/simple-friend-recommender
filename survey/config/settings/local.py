1ec4161f2a13cf22b2ac8c374c62b00e3281a33b
zkan <zkan.cs@gmail.com>
2021-04-04 11:16:54 +0700

    Set up Docker compose for local :bear:

--- survey/config/settings/local.py
+++ survey/config/settings/local.py
@@ -1,1 +1,15 @@
+import os
+
 from .base import * # noqa
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+    }
+}

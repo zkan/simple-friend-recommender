c1c1b250f8fbd7876ba871cdff10c6d644627fe7
zkan <zkan.cs@gmail.com>
2021-04-04 20:24:34 +0700

    Use dict instead of {} to avoid sharing between all field instances :bear:

--- survey/survey/questions/models.py
+++ survey/survey/questions/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class SurveyResponse(models.Model):
     name = models.CharField(max_length=200)
-    answers = models.JSONField(default={})
+    answers = models.JSONField(default=dict)
